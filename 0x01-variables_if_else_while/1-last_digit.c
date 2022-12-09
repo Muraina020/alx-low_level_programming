0fae4ce153538bc08c53ec8ea95fde49c0d23bdf
Muraina020 <murainaboluwatife@gmail.com>
2022-12-09 10:39:15 -0800

    task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,8 +14,8 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
-	printf("Last digit of %d is %d", n, x);
-		if (x > 5)
+	printf("Last digit of %d is %d ", n, x);
+	if (x > 5)
 	{
 		printf("and is grater than 5");
 	}
