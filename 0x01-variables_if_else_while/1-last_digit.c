0645a85529d6c0ef03b695b7c7e789c27248bde3
Muraina020 <murainaboluwatife@gmail.com>
2022-12-09 10:23:13 -0800

    commit1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	x = n % 10;
 	printf("Last digit of %d is %d", n, x);
 		if (x > 5)
-		{ 
-			printf("and is grater than 5");
-		}
+	{
+		printf("and is grater than 5");
+	}
 	if (x == 0)
 	{
 		printf("and is 0");
@@ -27,6 +27,6 @@ int main(void)
 	{
 		printf("and is less than 6 and not 0");
 	}
-	printf ("\n");
+	printf("\n");
 	return (0);
 }
